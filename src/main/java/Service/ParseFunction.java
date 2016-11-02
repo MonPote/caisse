b15ed51cde0e61ed19b05ea0861177d0d92dbba9
Steven Ghirighini <ghirig_s@epita.fr>
2016-11-02 19:53:40 +0100

    [ADDED] Message controller

---
+++ src/main/java/Service/ParseFunction.java
@@ -0,0 +1,39 @@
+package Service;
+
+/**
+ * Created by Steven on 02/11/2016.
+ */
+public class ParseFunction {
+    String fct = "";
+    Data data = new Data("", new String[0]);
+
+    public ParseFunction(String fct, Data data) {
+        this.fct = fct;
+        this.data = data;
+    }
+
+    public String getFct() {
+        return fct;
+    }
+
+    public void setFct(String fct) {
+        this.fct = fct;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public void execute() {
+        switch (fct) {
+            case "fct": {
+                // fct(data);
+                break;
+            }
+        }
+    }
+}
--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -3,6 +3,7 @@ package controller;
 import Service.Data;
 import Service.Handshake;
 import Service.HandshakeConnection;
+import Service.ParseFunction;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
@@ -10,10 +11,16 @@ import com.google.gson.GsonBuilder;
 import org.hibernate.boot.registry.selector.StrategyRegistration;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.springframework.expression.ParseException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 
@@ -23,6 +30,8 @@ import java.io.IOException;
  */
 @Controller
 public class HomeController {
+
+    ParseFunction fctn = new ParseFunction("", new Data("", new String[0]));
 
     @RequestMapping(value = {"/", "/home"})
     public String home(Model model) throws JsonProcessingException {
@@ -48,5 +57,22 @@ public class HomeController {
         model.addAttribute("strg", str);
 
         return "home";
+    }
+
+    @RequestMapping(value = "/app/api/msg", method = RequestMethod.GET)
+    public void getMessageGET(
+            @RequestParam(required = true) String fct,
+            BindingResult result, HttpServletRequest request)
+            throws ParseException {
+                fctn.setFct(fct);
+    }
+
+    @RequestMapping(value = "/app/api/msg", method = RequestMethod.POST)
+    public void getMessagePOST(
+            @PathVariable("Data") Data data,
+            BindingResult result, HttpServletRequest request)
+            throws ParseException {
+                fctn.setData(data);
+                fctn.execute();
     }
 }
